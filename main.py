9cea13c84ae6f68f76a6696dd149c18b958febb1
RukshanRanasinghe2000 <112747422+RukshanRanasinghe2000@users.noreply.github.com>
2025-01-30 17:29:40 +0530

    chore: fix cors issue and initialize frontend directory. feat: Add frontend view for pass request to the backend.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.routes import router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all domains (for testing; restrict in production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix="/api", tags=["image_upload"])
